b907c0736bfeb2deb879828aac879a58be842623
Gavin Gilbert <gilbert.gavin@outlook.com>
2023-08-08 16:46:01 -0400

    Day 1 Complete

---
+++ src/main.rs
@@ -0,0 +1,21 @@
+use std::fs::File;
+use std::io::{self, BufRead};
+use std::path::Path;
+
+fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
+where P: AsRef<Path>, {
+    let file = File::open(filename)?;
+    Ok(io::BufReader::new(file).lines())
+}
+
+fn parse_input() {
+    if let Ok(lines) = read_lines("test.txt") {
+        for line in lines {
+			// code here
+        }
+    }
+}
+
+fn main() {
+	parse_input();
+}
