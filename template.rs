3ff5769d6217066a7826a88e004a89516f8b3614
Gavin Gilbert <gilbert.gavin@outlook.com>
2023-08-14 17:16:47 -0400

    Update template.rs
    Clippy saves the day:
    - https://rust-lang.github.io/rust-clippy/master/index.html#/manual_flatten

--- template.rs
+++ template.rs
@@ -3,19 +3,21 @@ use std::io::{self, BufRead};
 use std::path::Path;
 
 fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where P: AsRef<Path>, {
+where
+    P: AsRef<Path>,
+{
     let file = File::open(filename)?;
     Ok(io::BufReader::new(file).lines())
 }
 
 fn parse_input() {
     if let Ok(lines) = read_lines("test.txt") {
-        for line in lines {
-			// code here
+        for line in lines.into_iter().flatten() {
+            // code here
         }
     }
 }
 
 fn main() {
-	parse_input();
+    parse_input();
 }
