bd122612b1f2bfdc3e1b0f9865eeec0ca339c15f
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-04 18:14:29 -0400

    Day 1 Complete

--- 2022/rust/day-01/src/bin/part-2.rs
+++ 2022/rust/day-01/src/bin/part-2.rs
@@ -1,7 +1,7 @@
-use day_01::process_part1;
+use day_01::process_part2;
 use std::fs;
 
 fn main() {
     let file = fs::read_to_string("./input.txt").unwrap();
-    println!("{}", process_part1(&file));
+    println!("{}", process_part2(&file));
 }
--- 2022/rust/day-01/src/lib.rs
+++ 2022/rust/day-01/src/lib.rs
@@ -12,27 +12,49 @@ pub fn process_part1(input: &str) -> String {
     result.to_string()
 }
 
+pub fn process_part2(input: &str) -> String {
+    let mut result = input
+        .split("\n\n")
+        .map(|elf_load| {
+            elf_load
+                .lines()
+                .map(|item| item.parse::<u32>().unwrap())
+                .sum::<u32>()
+        })
+        .collect::<Vec<_>>();
+    result.sort_by(|a, b| b.cmp(a));
+    let sum: u32 = result.iter().take(3).sum();
+    sum.to_string()
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "1000
+3000
+2000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000";
+
     #[test]
-    fn it_works() {
-        let input = "1000
-		2000
-		3000
-		
-		4000
-		
-		5000
-		6000
-		
-		7000
-		8000
-		9000
-		
-		10000";
-        let result = process_part1(input);
+    fn part1_works() {
+        let result = process_part1(INPUT);
         assert_eq!(result, "24000");
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "45000");
     }
 }
