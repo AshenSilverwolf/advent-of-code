ee979a5deaf107b0b6bd1ae2e7b7ac6b2b99e729
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-04 18:27:07 -0400

    Day 2 Part 1 Progress
    - foundations set for the day's project
    - enum Move

--- 2022/rust/day-02/src/lib.rs
+++ 2022/rust/day-02/src/lib.rs
@@ -1,14 +1,61 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::str::FromStr;
+
+enum Move {
+    Rock,
+    Paper,
+    Scissors,
+}
+
+impl PartialOrd for Move {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        todo!()
+    }
+}
+
+impl FromStr for Move {
+    type Err = String;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
+            _ => Err("Invalid Move".to_string()),
+        }
+    }
+}
+
+pub fn process_part1(input: &str) -> String {
+    input.lines().map(|line| {
+        let moves: Vec<Move> = line
+            .split(" ")
+            .map(|s| s.parse::<Move>().unwrap())
+            .collect();
+    });
+    "one".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "two".to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "A Y
+B X
+C Z";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, "15");
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "12");
     }
 }
