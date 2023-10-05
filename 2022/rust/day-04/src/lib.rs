5eef5941211277264867dace658b14cb5b8de4b8
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-05 18:22:36 -0400

    Day 4 Setup

--- 2022/rust/day-04/src/lib.rs
+++ 2022/rust/day-04/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn process_part1(input: &str) -> String {
+    "one".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "two".to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part1_works() {
+        let result = process_part1(INPUT);
+        assert_eq!(result, "2");
+    }
+
+    #[test]
+    fn part2_works() {
+        let result = process_part2(INPUT);
+        assert_eq!(result, "4");
     }
 }
