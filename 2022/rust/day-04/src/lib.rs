72a30ed946bece6317e01d229007eb0abe4d7f87
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-05 19:56:39 -0400

    Day 4 Part 1 Functional
    functional, but it's a huge map()
    would like to slim it down later

--- 2022/rust/day-04/src/lib.rs
+++ 2022/rust/day-04/src/lib.rs
@@ -1,5 +1,28 @@
+use std::collections::HashSet;
+
 pub fn process_part1(input: &str) -> String {
-    "one".to_string()
+    let result: u32 = input
+        .lines()
+        .map(|s| {
+            let mut zones = s.split(',');
+            let first: &str = zones.next().unwrap();
+            let second: &str = zones.next().unwrap();
+            let mut zone_one = first.split('-');
+            let mut zone_two = second.split('-');
+            let one_one: u32 = zone_one.next().unwrap().parse::<u32>().unwrap();
+            let one_two: u32 = zone_one.next().unwrap().parse::<u32>().unwrap();
+            let two_one: u32 = zone_two.next().unwrap().parse::<u32>().unwrap();
+            let two_two: u32 = zone_two.next().unwrap().parse::<u32>().unwrap();
+            let set_one: HashSet<u32> = (one_one..=one_two).collect::<HashSet<u32>>();
+            let set_two: HashSet<u32> = (two_one..=two_two).collect::<HashSet<u32>>();
+            if set_one.is_superset(&set_two) || set_two.is_superset(&set_one) {
+                1
+            } else {
+                0
+            }
+        })
+        .sum();
+    result.to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
