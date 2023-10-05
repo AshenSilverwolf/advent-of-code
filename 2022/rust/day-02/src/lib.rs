0109df552cf841fa3d99e484a068c813e5f6b86d
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-04 23:10:16 -0400

    Day 2 Part 1 Functional
    not correct, but functional

--- 2022/rust/day-02/src/lib.rs
+++ 2022/rust/day-02/src/lib.rs
@@ -1,14 +1,26 @@
-use std::str::FromStr;
+use std::{cmp::Ordering, str::FromStr};
+use Ordering::*;
 
+#[derive(PartialEq, Clone, Copy)]
 enum Move {
-    Rock,
-    Paper,
-    Scissors,
+    Rock = 1,
+    Paper = 2,
+    Scissors = 3,
 }
 
 impl PartialOrd for Move {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        todo!()
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        match (self, other) {
+            (&Move::Rock, &Move::Rock) => Some(Equal),
+            (&Move::Rock, &Move::Paper) => Some(Less),
+            (&Move::Rock, &Move::Scissors) => Some(Greater),
+            (&Move::Paper, &Move::Rock) => Some(Greater),
+            (&Move::Paper, &Move::Paper) => Some(Equal),
+            (&Move::Paper, &Move::Scissors) => Some(Less),
+            (&Move::Scissors, &Move::Rock) => Some(Less),
+            (&Move::Scissors, &Move::Paper) => Some(Greater),
+            (&Move::Scissors, &Move::Scissors) => Some(Equal),
+        }
     }
 }
 
@@ -20,22 +32,31 @@ impl FromStr for Move {
             "A" | "X" => Ok(Move::Rock),
             "B" | "Y" => Ok(Move::Paper),
             "C" | "Z" => Ok(Move::Scissors),
-            _ => Err("Invalid Move".to_string()),
+            _ => Err(String::from("Invalid Move")),
         }
     }
 }
 
 pub fn process_part1(input: &str) -> String {
-    input.lines().map(|line| {
-        let moves: Vec<Move> = line
-            .split(" ")
-            .map(|s| s.parse::<Move>().unwrap())
-            .collect();
-    });
-    "one".to_string()
+    let result: u32 = input
+        .lines()
+        .map(|line| {
+            let moves: Vec<Move> = line
+                .split(' ')
+                .map(|s| s.parse::<Move>().unwrap())
+                .collect();
+            match moves[0].partial_cmp(&moves[1]) {
+                Some(Equal) => 3 + moves[1] as u32,
+                Some(Less) => moves[1] as u32,
+                Some(Greater) => 6 + moves[1] as u32,
+                None => panic!("Moves should be comparable"),
+            }
+        })
+        .sum();
+    result.to_string()
 }
 
-pub fn process_part2(input: &str) -> String {
+pub fn process_part2(_input: &str) -> String {
     "two".to_string()
 }
 
