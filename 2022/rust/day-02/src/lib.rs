90a928cc8df863d4dedee41fa8aeca98038568af
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-05 15:22:16 -0400

    Day 2 Complete
    switched back to my original implementation for part 1
    don't like stealing work

--- 2022/rust/day-02/src/lib.rs
+++ 2022/rust/day-02/src/lib.rs
@@ -1,20 +1,10 @@
-use std::{cmp::Ordering, str::FromStr};
+use std::str::FromStr;
 
-#[derive(PartialEq, Copy, Clone)]
+#[derive(Copy, Clone)]
 enum Move {
     Rock = 1,
     Paper = 2,
     Scissors = 3,
-}
-
-impl PartialOrd for Move {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        match (*self, *other) {
-            (Move::Scissors, Move::Rock) => Some(Ordering::Less),
-            (Move::Rock, Move::Scissors) => Some(Ordering::Greater),
-            _ => Some((*self as u8).cmp(&(*other as u8))),
-        }
-    }
 }
 
 impl FromStr for Move {
@@ -22,28 +12,28 @@ impl FromStr for Move {
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match s {
-            "A" => Ok(Move::Rock),
-            "B" => Ok(Move::Paper),
-            "C" => Ok(Move::Scissors),
+            "A" | "X" => Ok(Move::Rock),
+            "B" | "Y" => Ok(Move::Paper),
+            "C" | "Z" => Ok(Move::Scissors),
             _ => Err(String::from("Invalid Move")),
         }
     }
 }
 
-enum Result {
+enum Outcome {
     Win = 6,
     Draw = 3,
     Loss = 0,
 }
 
-impl FromStr for Result {
+impl FromStr for Outcome {
     type Err = String;
 
     fn from_str(s: &str) -> std::result::Result<Self, Self::Err> {
         match s {
-            "X" => Ok(Result::Loss),
-            "Y" => Ok(Result::Draw),
-            "Z" => Ok(Result::Win),
+            "X" => Ok(Outcome::Loss),
+            "Y" => Ok(Outcome::Draw),
+            "Z" => Ok(Outcome::Win),
             _ => Err(String::from("Invalid Result")),
         }
     }
@@ -57,19 +47,43 @@ pub fn process_part1(input: &str) -> String {
                 .split(' ')
                 .map(|s| s.parse::<Move>().unwrap())
                 .collect();
-            match moves[0].partial_cmp(&moves[1]) {
-                Some(Ordering::Equal) => 3 + moves[1] as u32,
-                Some(Ordering::Less) => 6 + moves[1] as u32,
-                Some(Ordering::Greater) => moves[1] as u32,
-                None => panic!("Moves should be comparable"),
+            match (moves[0], moves[1]) {
+                (Move::Rock, Move::Rock) => moves[1] as u32 + 3,
+                (Move::Rock, Move::Paper) => moves[1] as u32 + 6,
+                (Move::Rock, Move::Scissors) => moves[1] as u32,
+                (Move::Paper, Move::Rock) => moves[1] as u32,
+                (Move::Paper, Move::Paper) => moves[1] as u32 + 3,
+                (Move::Paper, Move::Scissors) => moves[1] as u32 + 6,
+                (Move::Scissors, Move::Rock) => moves[1] as u32 + 6,
+                (Move::Scissors, Move::Paper) => moves[1] as u32,
+                (Move::Scissors, Move::Scissors) => moves[1] as u32 + 3,
             }
         })
         .sum();
     result.to_string()
 }
 
-pub fn process_part2(_input: &str) -> String {
-    "two".to_string()
+pub fn process_part2(input: &str) -> String {
+    let result: u32 = input
+        .lines()
+        .map(|line| {
+            let items: Vec<&str> = line.split(' ').collect();
+            let elf: Move = items[0].parse().unwrap();
+            let outcome: Outcome = items[1].parse().unwrap();
+            match (elf, outcome) {
+                (Move::Rock, Outcome::Win) => Move::Paper as u32 + 6,
+                (Move::Rock, Outcome::Draw) => Move::Rock as u32 + 3,
+                (Move::Rock, Outcome::Loss) => Move::Scissors as u32,
+                (Move::Paper, Outcome::Win) => Move::Scissors as u32 + 6,
+                (Move::Paper, Outcome::Draw) => Move::Paper as u32 + 3,
+                (Move::Paper, Outcome::Loss) => Move::Rock as u32,
+                (Move::Scissors, Outcome::Win) => Move::Rock as u32 + 6,
+                (Move::Scissors, Outcome::Draw) => Move::Scissors as u32 + 3,
+                (Move::Scissors, Outcome::Loss) => Move::Paper as u32,
+            }
+        })
+        .sum();
+    result.to_string()
 }
 
 #[cfg(test)]
@@ -87,7 +101,6 @@ C Z";
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
         let result = process_part2(INPUT);
         assert_eq!(result, "12");
