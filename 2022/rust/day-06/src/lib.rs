299842940c13cb96e870bf9b041c9843cc38df55
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-10 14:28:05 +0000

    Day 6 Complete
    This one was very simple, once you found out about windows.
    Turns out you can iterate over an overlapping sequence of 4 byte-characters, collect them into a set, and then compare the length of the set against the length of the window.
    Thanks again to Chris Biscardi

--- 2022/rust/day-06/src/lib.rs
+++ 2022/rust/day-06/src/lib.rs
@@ -1,29 +1,50 @@
+use std::collections::HashSet;
+
 pub fn process_part1(input: &str) -> String {
-    let result = "one";
-    result.to_string()
+    let bytes = input.bytes().collect::<Vec<u8>>();
+    let sequence = bytes
+        .windows(4)
+        .enumerate()
+        .find(|(_i, slice)| {
+            let set = slice.iter().collect::<HashSet<&u8>>();
+            slice.len() == set.len()
+        })
+        .unwrap();
+    (sequence.0 + 1 + 3).to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    let result = "two";
-    result.to_string()
+    let bytes = input.bytes().collect::<Vec<u8>>();
+    let sequence = bytes
+        .windows(14)
+        .enumerate()
+        .find(|(_i, slice)| {
+            let set = slice.iter().collect::<HashSet<&u8>>();
+            slice.len() == set.len()
+        })
+        .unwrap();
+    (sequence.0 + 1 + 13).to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    const INPUT: &str = "mjqjpqmgbljsphdztnvjfqwrcgsmlb";
-
     #[test]
     fn part1_works() {
-        let result = process_part1(INPUT);
-        assert_eq!(result, "7");
+        assert_eq!(process_part1("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), "7");
+        assert_eq!(process_part1("bvwbjplbgvbhsrlpgdmjqwftvncz"), "5");
+        assert_eq!(process_part1("nppdvjthqldpwncqszvftbrmjl"), "6");
+        assert_eq!(process_part1("nznrnfrfntjfmvfwmzdfjlvtqnbhcprs"), "10");
+        assert_eq!(process_part1("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), "11");
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
-        let result = process_part2(INPUT);
-        assert_eq!(result, "19");
+        assert_eq!(process_part2("mjqjpqmgbljsphdztnvjfqwrcgsmlb"), "19");
+        assert_eq!(process_part2("bvwbjplbgvbhsrlpgdmjqwftvncz"), "23");
+        assert_eq!(process_part2("nppdvjthqldpwncqszvftbrmjlhg"), "23");
+        assert_eq!(process_part2("nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg"), "29");
+        assert_eq!(process_part2("zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw"), "26");
     }
 }
