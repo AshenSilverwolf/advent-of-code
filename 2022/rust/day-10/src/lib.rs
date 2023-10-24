1fad279da2638ba1f6deb584173a7b7f9cc691ff
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-24 18:37:15 -0400

    Day 10 Part 2 Progress
    functional, now able to test output against test

--- 2022/rust/day-10/src/lib.rs
+++ 2022/rust/day-10/src/lib.rs
@@ -1,14 +1,17 @@
-use nom::branch::alt;
-use nom::bytes::complete::tag;
-use nom::character::complete::{self, digit1, newline};
-use nom::combinator::{map_res, opt};
-use nom::multi::separated_list1;
-use nom::*;
+use nom::{
+    branch::alt,
+    bytes::complete::tag,
+    character::complete::{self, newline},
+    multi::separated_list1,
+    *,
+};
+use std::collections::HashMap;
 
-#[derive(Debug)]
+#[derive(Debug, Clone, PartialEq)]
 enum Operation {
     Add(i32),
     Noop,
+    Empty,
 }
 use nom::sequence::preceded;
 use Operation::*;
@@ -29,21 +32,21 @@ fn operations(input: &str) -> IResult<&str, Vec<Operation>> {
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_, operations) = operations(input).unwrap(); // valid
+    let (_, operations) = operations(input).unwrap();
     let key_cycles = [20, 60, 100, 140, 180, 220];
-    let mut key_values = vec![];
+    let mut key_cycle_values: HashMap<i32, i32> = HashMap::new();
     let mut register: i32 = 1;
 
     let mut cycle: i32 = 0;
     for op in operations {
         cycle += 1;
         if key_cycles.contains(&cycle) {
-            key_values.push((register, cycle));
+            key_cycle_values.insert(cycle, register);
         }
         if let Add(num) = op {
             cycle += 1;
             if key_cycles.contains(&cycle) {
-                key_values.push((register, cycle));
+                key_cycle_values.insert(cycle, register);
             }
             register += num;
         }
@@ -52,41 +55,69 @@ pub fn process_part1(input: &str) -> String {
         }
     }
 
-    let output: i32 = key_values.iter().map(|(val, cycle)| val * cycle).sum();
+    key_cycle_values
+        .iter()
+        .map(|(cycle, register)| cycle * register)
+        .sum::<i32>()
+        .to_string()
+}
 
-    output.to_string()
+fn draw(crt_display: &mut [Vec<char>], cycle: &i32, register: &i32) {
+    let pixels = (register - 1)..(register + 1);
+    let row = (cycle / 40) as usize;
+    let col = (cycle % 40) as usize;
+    dbg!(format!("{}, {}, {}", row, col, cycle));
+    crt_display[row].push(match pixels.contains(cycle) {
+        true => '#',
+        false => '.',
+    });
 }
 
 pub fn process_part2(input: &str) -> String {
+    let mut crt_display: Vec<Vec<char>> = Vec::with_capacity(6);
+    for _ in 0..6 {
+        crt_display.push(Vec::with_capacity(40));
+    }
     let operations = operations(input).unwrap().1;
-    let mut ops_iter = operations.into_iter();
+    let mut ops_iter = operations.into_iter().peekable();
     let mut register: i32 = 1;
     let mut cycle = 0;
-    let mut state = Noop;
-    let mut adding = false;
-    let mut crt_pixels: Vec<&str> = vec![];
+    let mut curr_op = ops_iter.next().unwrap();
+    let mut last_op = Empty;
 
-    while cycle <= 240 {
+    while cycle < 240 {
+        //* If new op, Begin execution of operation
+        // if curr_op != last_op {
+        //     // match curr_op {
+        //     //     Add(num) => {}
+        //     //     _ => {}
+        //     // }
+        // }
+
+        //* Draw on CRT
+        draw(&mut crt_display, &cycle, &register);
+
+        //* increment the cycle; finish execution of operation
         cycle += 1;
-        // START
-        match (state, adding) {
-            (Noop, _) => {
-                state = ops_iter.next().unwrap();
+        let second_cycle = last_op == curr_op;
+        last_op = curr_op.clone();
+        if let Noop = curr_op {
+            if let Some(new_op) = ops_iter.next() {
+                curr_op = new_op;
             }
-            (Add(num), false) => {
-                state = ops_iter.next().unwrap();
+        } else if second_cycle {
+            let Add(x) = curr_op else { unreachable!() };
+            register += x;
+            if let Some(new_op) = ops_iter.next() {
+                curr_op = new_op;
             }
-            _ => {}
-        };
-        // DURING
-
-        // END
-
-        state = ops_iter.next().unwrap();
-        let sprite = [register - 1, register, register + 1];
+        }
     }
 
-    "two".to_string()
+    crt_display
+        .iter()
+        .map(|row| row.iter().collect::<String>())
+        .fold(String::new(), |acc, elem| acc + &elem + "\n")
 }
 
 #[cfg(test)]
@@ -253,7 +284,7 @@ noop";
     }
 
     #[test]
-    #[ignore]
+    // #[ignore]
     fn part2_works() {
         assert_eq!(process_part2(INPUT), OUTPUT.to_string());
     }
