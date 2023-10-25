8f924f7fc0501359f9117614fbfb2e5f099c3a76
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-25 14:10:59 +0000

    Day 11 Setup

--- 2022/rust/day-11/src/lib.rs
+++ 2022/rust/day-11/src/lib.rs
@@ -1,14 +1,53 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use nom::*;
+
+pub fn process_part1(input: &str) -> String {
+    "one".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "two".to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part1_works() {
+        assert_eq!(process_part1(INPUT), "10605");
+    }
+
+    #[test]
+    #[ignore]
+    fn part2_works() {
+        assert_eq!(process_part2(INPUT), "2713310158");
     }
 }
