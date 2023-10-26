08489c185bfcf7c78d2ef18e7959452b080c0a3b
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-26 15:01:37 +0000

    Day 12 Setup

--- 2022/rust/day-12/src/lib.rs
+++ 2022/rust/day-12/src/lib.rs
@@ -1,14 +1,29 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+pub fn process_part1(input: &str) -> String {
+    "one".to_string()
+}
+
+pub fn process_part2(input: &str) -> String {
+    "two".to_string()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    const INPUT: &str = "Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi";
+
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn part1_works() {
+        assert_eq!(process_part1(INPUT), "31");
+    }
+
+    #[test]
+    #[ignore]
+    fn part2_works() {
+        assert_eq!(process_part2(INPUT), "29");
     }
 }
