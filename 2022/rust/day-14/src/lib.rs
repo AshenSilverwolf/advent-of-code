cd502b3d6db5cc4d304067ba9acd9c8c90e6b498
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-10-30 13:29:51 -0400

    Day 14 Complete
    solution courtesy of chris biscardi
    way more lightweight than my own solution

--- 2022/rust/day-14/src/lib.rs
+++ 2022/rust/day-14/src/lib.rs
@@ -4,7 +4,7 @@ use nom::{
     character::complete::{self, line_ending},
     multi::separated_list1,
     sequence::separated_pair,
-    IResult, Parser, *,
+    IResult,
 };
 use std::collections::BTreeSet;
 
@@ -40,13 +40,27 @@ fn rocks(input: &str) -> IResult<&str, BTreeSet<(u32, u32)>> {
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_input, rocks) = rocks(input).unwrap();
+    let (_input, mut rocks) = rocks(input).unwrap();
+
+    let mut rocks_vec: Vec<&(u32, u32)> = rocks.iter().collect();
+    rocks_vec.sort_by(|a, b| a.1.cmp(&b.1));
+    let lowest_rock = **rocks_vec.last().unwrap();
+    drop(rocks_vec);
+
+    let rock_count = rocks.len();
     let mut current_sand = (500, 0);
     loop {
+        if current_sand.1 > lowest_rock.1 {
+            break;
+        }
         let down = (current_sand.0, current_sand.1 + 1);
         let down_left = (current_sand.0 - 1, current_sand.1 + 1);
         let down_right = (current_sand.0 + 1, current_sand.1 + 1);
-        match (rocks.get(down), rocks.get(down_left), rocks.get(down_right)) {
+        match (
+            rocks.get(&down),
+            rocks.get(&down_left),
+            rocks.get(&down_right),
+        ) {
             (None, _, _) => {
                 current_sand = down;
             }
@@ -57,19 +71,69 @@ pub fn process_part1(input: &str) -> String {
                 current_sand = down_right;
             }
             (Some(_), Some(_), Some(_)) => {
-                // lock in place
+                rocks.insert(current_sand);
                 current_sand = (500, 0);
-            }
-            _ => {
-                panic!();
             }
         };
     }
-    todo!("one")
+
+    (rocks.len() - rock_count).to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
-    "two".to_string()
+    let (_input, mut rocks) = rocks(input).unwrap();
+
+    let mut rocks_vec: Vec<&(u32, u32)> = rocks.iter().collect();
+    rocks_vec.sort_by(|a, b| a.1.cmp(&b.1));
+    let lowest_rock = **rocks_vec.last().unwrap();
+    drop(rocks_vec);
+
+    let rock_count = rocks.len();
+    let mut current_sand = (500, 0);
+    while rocks.get(&(500, 0)).is_none() {
+        let down = (current_sand.0, current_sand.1 + 1);
+        let down_left = (current_sand.0 - 1, current_sand.1 + 1);
+        let down_right = (current_sand.0 + 1, current_sand.1 + 1);
+        match (
+            rocks.get(&down).or({
+                if down.1 == lowest_rock.1 + 2 {
+                    Some(&lowest_rock)
+                } else {
+                    None
+                }
+            }),
+            rocks.get(&down_left).or({
+                if down_left.1 == lowest_rock.1 + 2 {
+                    Some(&lowest_rock)
+                } else {
+                    None
+                }
+            }),
+            rocks.get(&down_right).or({
+                if down_right.1 == lowest_rock.1 + 2 {
+                    Some(&lowest_rock)
+                } else {
+                    None
+                }
+            }),
+        ) {
+            (None, _, _) => {
+                current_sand = down;
+            }
+            (_, None, _) => {
+                current_sand = down_left;
+            }
+            (_, _, None) => {
+                current_sand = down_right;
+            }
+            (Some(_), Some(_), Some(_)) => {
+                rocks.insert(current_sand);
+                current_sand = (500, 0);
+            }
+        };
+    }
+
+    (rocks.len() - rock_count).to_string()
 }
 
 #[cfg(test)]
@@ -85,7 +149,6 @@ mod tests {
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
         assert_eq!(process_part2(INPUT), "93");
     }
