e5c093c38532ff5fc10859afac6d204d0af29bc7
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-11-02 20:54:52 -0400

    Parsing

--- 2022/rust/day-16/src/lib.rs
+++ 2022/rust/day-16/src/lib.rs
@@ -1,5 +1,60 @@
+use nom::{
+    branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::{self, line_ending, newline},
+    multi::separated_list1,
+    sequence::{delimited, preceded, tuple},
+    IResult,
+};
+
+#[derive(Debug)]
+struct Valve<'a> {
+    name: &'a str,
+    flow_rate: u32,
+    tunnels: Vec<&'a str>,
+}
+
+fn parse_valve(input: &str) -> IResult<&str, &str> {
+    preceded(tag("Valve "), take(2_usize))(input)
+}
+
+fn parse_flow_rate(input: &str) -> IResult<&str, u32> {
+    preceded(tag(" has flow rate="), complete::u32)(input)
+}
+
+fn parse_tunnels(input: &str) -> IResult<&str, Vec<&str>> {
+    preceded(
+        alt((
+            tag("; tunnels lead to valves "),
+            tag("; tunnel leads to valve "),
+        )),
+        separated_list1(tag(", "), take(2_usize)),
+    )(input)
+}
+
+fn parse_line(input: &str) -> IResult<&str, Valve> {
+    let (input, (name, flow_rate, tunnels)) =
+        tuple((parse_valve, parse_flow_rate, parse_tunnels))(input)?;
+
+    Ok((
+        input,
+        Valve {
+            name,
+            flow_rate,
+            tunnels,
+        },
+    ))
+}
+
+fn parse_valves(input: &str) -> IResult<&str, Vec<Valve>> {
+    separated_list1(line_ending, parse_line)(input)
+}
+
 pub fn process_part1(input: &str) -> String {
-    todo!("one")
+    let (_input, valves) = parse_valves(input).unwrap();
+    dbg!(valves);
+
+    todo!("process_part1")
 }
 
 pub fn process_part2(input: &str) -> String {
