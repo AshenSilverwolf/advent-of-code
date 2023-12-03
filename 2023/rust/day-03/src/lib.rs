c41c664e646e3243bc08eb66a2fe1cabbd97f189
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-12-03 14:04:26 -0500

    2023 Day 3 Part 1 Complete
    all chris biscardi
    this is terribly complicated
    part of me has lost interest in this problem
    matches!() macro is pretty cool though

--- 2023/rust/day-03/src/lib.rs
+++ 2023/rust/day-03/src/lib.rs
@@ -1,8 +1,107 @@
-pub fn process_part1(input: &str) -> String {
-    todo!()
+use itertools::Itertools;
+use std::collections::BTreeMap;
+
+#[derive(Debug)]
+enum Value {
+    Symbol(char),
+    Number(u32),
+    Empty,
 }
 
-pub fn process_part2(input: &str) -> String {
+pub fn process_part1(input: &str) -> String {
+    let map = input
+        .lines()
+        .enumerate()
+        .flat_map(|(y, line)| {
+            line.chars().enumerate().map(move |(x, c)| {
+                (
+                    (y as i32, x as i32),
+                    match c {
+                        '.' => Value::Empty,
+                        c if c.is_ascii_digit() => {
+                            Value::Number(c.to_digit(10).expect("should be a number"))
+                        }
+                        c => Value::Symbol(c),
+                    },
+                )
+            })
+        })
+        .collect::<BTreeMap<(i32, i32), Value>>();
+    // dbg!(map);
+
+    let mut numbers: Vec<Vec<((i32, i32), u32)>> = vec![];
+    for ((y, x), value) in map.iter() {
+        if let Value::Number(num) = value {
+            match numbers.iter().last() {
+                Some(v) => {
+                    let last_num = v.iter().last();
+                    match last_num {
+                        Some(((last_num_x, _), _)) => {
+                            if last_num_x + 1 == *x {
+                                let last = numbers.iter_mut().last().expect("should exist");
+                                last.push(((*x, *y), *num));
+                            } else {
+                                numbers.push(vec![((*x, *y), *num)]);
+                            }
+                        }
+                        None => unimplemented!("shouldn't happen"),
+                    }
+                }
+                None => {
+                    numbers.push(vec![((*x, *y), *num)]);
+                }
+            }
+        }
+    }
+
+    // map: entire grid
+    // numbers: grouped sequential numbers
+    let mut total = 0;
+    for num_list in numbers {
+        // positions adjacent to a number, including itself
+        let positions = [
+            (1, 0),
+            (1, -1),
+            (1, 1),
+            (0, 0),
+            (0, -1),
+            (0, 1),
+            (-1, 0),
+            (-1, -1),
+            (-1, 1),
+        ];
+        let num_positions: Vec<(i32, i32)> = num_list.iter().map(|((y, x), _)| (*x, *y)).collect();
+        let pos_to_check: Vec<(i32, i32)> = num_list
+            .iter()
+            .flat_map(|(pos, _)| {
+                positions.iter().map(|outer_pos| {
+                    // outer_pos.x + pos.x, .y + .y
+                    (outer_pos.0 + pos.1, outer_pos.1 + pos.0)
+                })
+            })
+            .unique()
+            .filter(|num| !num_positions.contains(num))
+            .collect();
+
+        let is_part_number = pos_to_check.iter().any(|pos| {
+            let value = map.get(pos);
+            matches!(value, Some(Value::Symbol(_)))
+        });
+
+        if is_part_number {
+            total += num_list
+                .iter()
+                .map(|(_, num)| num.to_string())
+                .collect::<String>()
+                .parse::<u32>()
+                .unwrap();
+        }
+    }
+
+    total.to_string()
+}
+
+pub fn process_part2(_input: &str) -> String {
     todo!()
 }
 
@@ -10,16 +109,26 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "467..114..
+...*......
+..35..633.
+......#...
+617*......
+.....+.58.
+..592.....
+......755.
+...$.*....
+.664.598..";
 
     #[test]
     fn part1_works() {
-        let expected = String::from("");
+        let expected = String::from("4361");
         let result = process_part1(INPUT);
         assert_eq!(expected, result);
     }
 
     #[test]
+    #[ignore]
     fn part2_works() {
         let expected = String::from("");
         let result = process_part2(INPUT);
