69c4dfce85966256286f6129ff22dab5795c8100
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-12-16 02:46:37 -0500

    Day 7 Part 1 Complete
    works beautifully
    very happy with the implementation
    part 2 ruins it all because J is now Joker
    so now i have to modify all my impls to account for that
    modules the answer???

--- 2023/rust/day-07/src/lib.rs
+++ 2023/rust/day-07/src/lib.rs
@@ -1,127 +1,280 @@
-use std::cmp::Ordering;
+// Modules?????
+
 use nom::{
-    IResult,
-    character::complete::{self, newline, one_of},
     bytes::complete::tag,
-    multi::{many1, separated_list1},
+    character::complete::{self, newline, one_of},
     combinator::map,
+    multi::{many1, separated_list1},
     sequence::separated_pair,
+    IResult,
 };
+use std::{cmp::Ordering, collections::BTreeMap};
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum Card {
-    Deuce,
-    Three,
-    Four,
-    Five,
-    Six,
-    Seven,
-    Eight,
-    Nine,
-    Ten,
-    Jack,
-    Queen,
-    King,
-    Ace,
-}
+// do not change
+mod p1_types {
+    use std::{cmp::Ordering, collections::BTreeMap};
 
-impl TryFrom<char> for Card {
-    type Error = &'static str;
-    
-    fn try_from(value: char) -> Result<Self, Self::Error> {
-        match value {
-            '2' => Ok(Card::Deuce),
-            '3' => Ok(Card::Three),
-            '4' => Ok(Card::Four),
-            '5' => Ok(Card::Five),
-            '6' => Ok(Card::Six),
-            '7' => Ok(Card::Seven),
-            '8' => Ok(Card::Eight),
-            '9' => Ok(Card::Nine),
-            'T' => Ok(Card::Ten),
-            'J' => Ok(Card::Jack),
-            'Q' => Ok(Card::Queen),
-            'K' => Ok(Card::King),
-            'A' => Ok(Card::Ace),
-            _ => Err("Invalid card value"),
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    pub enum Card {
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
+    }
+
+    impl TryFrom<char> for Card {
+        type Error = &'static str;
+
+        fn try_from(value: char) -> Result<Self, Self::Error> {
+            match value {
+                '2' => Ok(Card::Deuce),
+                '3' => Ok(Card::Three),
+                '4' => Ok(Card::Four),
+                '5' => Ok(Card::Five),
+                '6' => Ok(Card::Six),
+                '7' => Ok(Card::Seven),
+                '8' => Ok(Card::Eight),
+                '9' => Ok(Card::Nine),
+                'T' => Ok(Card::Ten),
+                'J' => Ok(Card::Jack),
+                'Q' => Ok(Card::Queen),
+                'K' => Ok(Card::King),
+                'A' => Ok(Card::Ace),
+                _ => Err("Invalid card value"),
+            }
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+    pub enum HandRank {
+        HighCard,
+        OnePair,
+        TwoPair,
+        Trips,
+        FullHouse,
+        Quads,
+        Quints,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct Hand(pub Vec<Card>);
+
+    impl Hand {
+        fn rank(&self) -> HandRank {
+            let mut card_quantities: BTreeMap<Card, u8> = BTreeMap::new();
+            for card in self.0.clone() {
+                if let Some(x) = card_quantities.get_mut(&card) {
+                    *x += 1;
+                } else {
+                    card_quantities.insert(card, 1);
+                }
+            }
+
+            let mut card_counts = card_quantities.values().cloned().collect::<Vec<u8>>();
+            card_counts.sort();
+            let mut card_counts_hi_to_lo = card_counts.into_iter().rev();
+
+            let Some(high) = card_counts_hi_to_lo.next() else {
+                unreachable!()
+            };
+            match high {
+                5 => HandRank::Quints,
+                4 => HandRank::Quads,
+                3 => {
+                    if let Some(2) = card_counts_hi_to_lo.next() {
+                        HandRank::FullHouse
+                    } else {
+                        HandRank::Trips
+                    }
+                }
+                2 => {
+                    if let Some(2) = card_counts_hi_to_lo.next() {
+                        HandRank::TwoPair
+                    } else {
+                        HandRank::OnePair
+                    }
+                }
+                1 => HandRank::HighCard,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    impl PartialOrd for Hand {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Ord for Hand {
+        fn cmp(&self, other: &Self) -> Ordering {
+            let cmp_result = self.rank().cmp(&other.rank());
+            match cmp_result {
+                Ordering::Equal => self.0.cmp(&other.0),
+                _ => cmp_result,
+            }
         }
     }
 }
 
-#[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
-enum HandRank {
-    HighCard,
-    OnePair,
-    TwoPair,
-    Trips,
-    FullHouse,
-    Quads,
-    Quints,
-}
+// TODO?: modify to account for Jokers in part 2
+mod p2_types {
+    use std::{cmp::Ordering, collections::BTreeMap};
 
-#[derive(Debug, PartialEq, Eq)]
-struct Hand(Vec<Card>);
-
-impl PartialOrd for Hand {
-    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
-        Some(self.cmp(other))
+    #[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord)]
+    pub enum Card {
+        Deuce,
+        Three,
+        Four,
+        Five,
+        Six,
+        Seven,
+        Eight,
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace,
     }
-}
 
-impl Ord for Hand {
-    fn cmp(&self, other: &Self) -> Ordering {
-        let cmp_result = self.rank().cmp(&other.rank());
-        match cmp_result {
-            Ordering::Equal => {
-                // ranks are same, evaluate card by card
-                return self.0.cmp(&other.0);
+    impl TryFrom<char> for Card {
+        type Error = &'static str;
+
+        fn try_from(value: char) -> Result<Self, Self::Error> {
+            match value {
+                '2' => Ok(Card::Deuce),
+                '3' => Ok(Card::Three),
+                '4' => Ok(Card::Four),
+                '5' => Ok(Card::Five),
+                '6' => Ok(Card::Six),
+                '7' => Ok(Card::Seven),
+                '8' => Ok(Card::Eight),
+                '9' => Ok(Card::Nine),
+                'T' => Ok(Card::Ten),
+                'J' => Ok(Card::Jack),
+                'Q' => Ok(Card::Queen),
+                'K' => Ok(Card::King),
+                'A' => Ok(Card::Ace),
+                _ => Err("Invalid card value"),
             }
-            _ => {
-                // ranks are different, return comparison
-                return cmp_result;
+        }
+    }
+
+    #[derive(Debug, PartialEq, Eq, PartialOrd, Ord)]
+    pub enum HandRank {
+        HighCard,
+        OnePair,
+        TwoPair,
+        Trips,
+        FullHouse,
+        Quads,
+        Quints,
+    }
+
+    #[derive(Debug, Clone, PartialEq, Eq)]
+    pub struct Hand(pub Vec<Card>);
+
+    impl Hand {
+        fn rank(&self) -> HandRank {
+            let mut card_quantities: BTreeMap<Card, u8> = BTreeMap::new();
+            for card in self.0.clone() {
+                if let Some(x) = card_quantities.get_mut(&card) {
+                    *x += 1;
+                } else {
+                    card_quantities.insert(card, 1);
+                }
+            }
+
+            let mut card_counts = card_quantities.values().cloned().collect::<Vec<u8>>();
+            card_counts.sort();
+            let mut card_counts_hi_to_lo = card_counts.into_iter().rev();
+
+            let Some(high) = card_counts_hi_to_lo.next() else {
+                unreachable!()
+            };
+            match high {
+                5 => HandRank::Quints,
+                4 => HandRank::Quads,
+                3 => {
+                    if let Some(2) = card_counts_hi_to_lo.next() {
+                        HandRank::FullHouse
+                    } else {
+                        HandRank::Trips
+                    }
+                }
+                2 => {
+                    if let Some(2) = card_counts_hi_to_lo.next() {
+                        HandRank::TwoPair
+                    } else {
+                        HandRank::OnePair
+                    }
+                }
+                1 => HandRank::HighCard,
+                _ => unreachable!(),
+            }
+        }
+    }
+
+    impl PartialOrd for Hand {
+        fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+            Some(self.cmp(other))
+        }
+    }
+
+    impl Ord for Hand {
+        fn cmp(&self, other: &Self) -> Ordering {
+            let cmp_result = self.rank().cmp(&other.rank());
+            match cmp_result {
+                Ordering::Equal => self.0.cmp(&other.0),
+                _ => cmp_result,
             }
         }
     }
 }
 
-impl Hand {
-    fn rank(&self) -> HandRank {
-        todo!()
-    }
+fn card(input: &str) -> IResult<&str, p1_types::Card> {
+    map(one_of("23456789TJQKA"), |c: char| {
+        p1_types::Card::try_from(c).expect("valid char")
+    })(input)
 }
 
-fn card(input: &str) -> IResult<&str, Card> {
-    map(
-        one_of("23456789TJQKA"),
-        |c: char| Card::try_from(c).expect("valid char")
-    )(input)
-}
-
-fn hand(input: &str) -> IResult<&str, Hand> {
-    map(
-        many1(card),
-        |cards: Vec<Card>| Hand(cards)
-    )(input)
+fn hand(input: &str) -> IResult<&str, p1_types::Hand> {
+    map(many1(card), |cards: Vec<p1_types::Card>| {
+        p1_types::Hand(cards)
+    })(input)
 }
 
 fn bid(input: &str) -> IResult<&str, u32> {
     complete::u32(input)
 }
 
-fn line(input: &str) -> IResult<&str, (Hand, u32)> {
+fn line(input: &str) -> IResult<&str, (p1_types::Hand, u32)> {
     separated_pair(hand, tag(" "), bid)(input)
 }
 
-fn parse_input(input: &str) -> IResult<&str, Vec<(Hand, u32)>> {
+fn parse_input(input: &str) -> IResult<&str, Vec<(p1_types::Hand, u32)>> {
     separated_list1(newline, line)(input)
 }
 
 pub fn process_part1(input: &str) -> String {
-    let (_, mut before) = parse_input(input).expect("valid input");
-    let after = before.sort_by_key(|(hand, _)| hand);
-    dbg!(before, after);
-
-    todo!()
+    let (_, mut hands_bids) = parse_input(input).expect("valid input");
+    hands_bids.sort_by_key(|(hand, _)| hand.clone());
+    hands_bids
+        .iter()
+        .enumerate()
+        .map(|(x, &(_, bid))| (x as u32 + 1) * bid)
+        .sum::<u32>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
@@ -155,8 +308,8 @@ QQQJA 483";
 
     #[test]
     fn cards_order() {
-        assert!(Card::Deuce < Card::Three);
-        assert!(Card::Ace > Card::Jack);
-        assert!(Card::Ten == Card::Ten);
+        assert!(p1_types::Card::Deuce < p1_types::Card::Three);
+        assert!(p1_types::Card::Ace > p1_types::Card::Jack);
+        assert!(p1_types::Card::Ten == p1_types::Card::Ten);
     }
 }
