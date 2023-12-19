7c9470d63e041d0fe5d407f0355fde4623715116
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-12-19 16:58:53 -0500

    2023 Rust Day 8 Part 2 - FAILED primes
    Vec<n> = # of steps to node ending in Z for each node ending in A
    for n in Vec<n>:
    - map to all prime factors
    mutiply prime factors of all n
    answer?
    
    incorrect, way too high

--- 2023/rust/day-08/src/lib.rs
+++ 2023/rust/day-08/src/lib.rs
@@ -1,13 +1,18 @@
+#![feature(isqrt)]
+
 use nom::{
-    IResult,
-    character::complete::{one_of, newline},
-    multi::{many1, separated_list1},
-    combinator::value,
-    sequence::{separated_pair, delimited, tuple},
-    bytes::complete::{tag, take},
     branch::alt,
+    bytes::complete::{tag, take},
+    character::complete::{newline, one_of},
+    combinator::value,
+    multi::{many1, separated_list1},
+    sequence::{delimited, separated_pair, tuple},
+    IResult,
 };
+use primes::factors;
 use std::collections::BTreeMap;
+
+type NodeLRMap<'a> = BTreeMap<&'a str, (&'a str, &'a str)>;
 
 #[derive(Debug, Clone)]
 enum Turn {
@@ -29,22 +34,15 @@ fn take3(input: &str) -> IResult<&str, &str> {
 fn node(input: &str) -> IResult<&str, (&str, (&str, &str))> {
     let (input, origin) = take3(input)?;
     let (input, _) = tag(" = ")(input)?;
-    let (input, (left, right)) = delimited(
-        tag("("),
-        separated_pair(
-            take3,
-            tag(", "),
-            take3,
-        ),
-        tag(")"),
-    )(input)?;
+    let (input, (left, right)) =
+        delimited(tag("("), separated_pair(take3, tag(", "), take3), tag(")"))(input)?;
 
     let output = (origin, (left, right));
 
     Ok((input, output))
 }
 
-fn parse_input(input: &str) -> IResult<&str, (Vec<Turn>, BTreeMap<&str, (&str, &str)>)> {
+fn parse_input(input: &str) -> IResult<&str, (Vec<Turn>, NodeLRMap)> {
     let (input, (turns, nodes)) = separated_pair(
         turns,
         tuple((newline, newline)),
@@ -58,11 +56,11 @@ fn parse_input(input: &str) -> IResult<&str, (Vec<Turn>, BTreeMap<&str, (&str, &
 
 pub fn process_part1(input: &str) -> String {
     let (_, (turns, nodes_map)) = parse_input(input).expect("good things");
-    
+
     let mut turns_iter = turns.iter().cycle();
     let mut steps: u32 = 0;
     let mut current_node = "AAA";
-    
+
     while current_node != "ZZZ" {
         let turn = turns_iter.next().expect("valid turn");
         let (left, right) = nodes_map.get(&current_node).expect("valid node");
@@ -77,7 +75,37 @@ pub fn process_part1(input: &str) -> String {
 }
 
 pub fn process_part2(input: &str) -> String {
-    todo!()
+    let (_, (turns, nodes_map)) = parse_input(input).expect("good things");
+
+    let turns_iter = turns.iter().cycle();
+    let current_nodes: Vec<&str> = nodes_map
+        .keys()
+        .cloned()
+        .filter(|s| s.ends_with('A'))
+        .collect();
+    dbg!(&current_nodes);
+
+    current_nodes
+        .iter()
+        .map(|node| {
+            let mut turns = turns_iter.clone();
+            let mut steps = 0;
+            let mut curr_node = *node;
+            while !curr_node.ends_with('Z') {
+                let turn = turns.next().expect("next turn");
+                let (left, right) = nodes_map.get(curr_node).expect("valid left right");
+                curr_node = match *turn {
+                    Turn::Left => *left,
+                    Turn::Right => *right,
+                };
+                steps += 1;
+            }
+            steps
+        })
+        .flat_map(factors)
+        .map(|n| n as u128)
+        .product::<u128>()
+        .to_string()
 }
 
 #[cfg(test)]
@@ -90,6 +118,17 @@ AAA = (BBB, BBB)
 BBB = (AAA, ZZZ)
 ZZZ = (ZZZ, ZZZ)";
 
+    const INPUT_2: &str = "LR
+
+11A = (11B, XXX)
+11B = (XXX, 11Z)
+11Z = (11B, XXX)
+22A = (22B, XXX)
+22B = (22C, 22C)
+22C = (22Z, 22Z)
+22Z = (22B, 22B)
+XXX = (XXX, XXX)";
+
     #[test]
     fn part1_works() {
         let expected = String::from("6");
@@ -98,10 +137,14 @@ ZZZ = (ZZZ, ZZZ)";
     }
 
     #[test]
-    #[ignore]
     fn part2_works() {
-        let expected = String::from("");
-        let result = process_part2(INPUT);
+        let expected = String::from("6");
+        let result = process_part2(INPUT_2);
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn prime_factorizations() {
+        assert_eq!(vec![2, 3, 17], factors(102));
     }
 }
