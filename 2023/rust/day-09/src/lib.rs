1f66422bf7f679360ab78e94a61fbe140d3c0f69
AshenSilverwolf <gilbert.gavin@outlook.com>
2023-12-20 15:24:40 +0000

    2023 Rust Day 9 Part 1 Complete

--- 2023/rust/day-09/src/lib.rs
+++ 2023/rust/day-09/src/lib.rs
@@ -1,5 +1,44 @@
+use nom::{
+    IResult,
+    character::complete::{self, newline},
+    multi::separated_list1,
+    bytes::complete::tag,
+};
+
+fn history(input: &str) -> IResult<&str, Vec<i32>> {
+    separated_list1(tag(" "), complete::i32)(input)
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<Vec<i32>>> {
+    separated_list1(newline, history)(input)
+}
+
+fn diff_tree(slice: &[i32]) -> i32 {
+    if slice.iter().all(|n| *n == 0) {
+        return 0;
+    }
+
+    let mut next = vec![];
+    for i in 1..(slice.len()) {
+        let mut slice_iter = slice[i-1..i+1].iter();
+        let x = slice_iter.next().expect("valid x");
+        let y = slice_iter.next().expect("valid y");
+        next.push(y-x);
+    }
+
+    let last_val = slice.iter().last().unwrap();
+    let next_val = last_val + diff_tree(&next);
+    next_val
+}
+
 pub fn process_part1(input: &str) -> String {
-    todo!()
+    let (_, histories) = parse_input(input).expect("valid input");
+
+    histories
+        .iter()
+        .map(|h| diff_tree(h))
+        .sum::<i32>()
+        .to_string()
 }
 
 pub fn process_part2(input: &str) -> String {
@@ -10,19 +49,30 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "0 3 6 9 12 15
+1 3 6 10 15 21
+10 13 16 21 30 45";
 
     #[test]
     fn part1_works() {
-        let expected = String::from("");
+        let expected = String::from("114");
         let result = process_part1(INPUT);
         assert_eq!(expected, result);
     }
 
     #[test]
+    #[ignore]
     fn part2_works() {
         let expected = String::from("");
         let result = process_part2(INPUT);
         assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn diff_tree_works() {
+        assert_eq!(4, diff_tree(&vec![1, 2, 3]));
+        assert_eq!(18, diff_tree(&vec![0,3,6,9,12,15]));
+        assert_eq!(28, diff_tree(&vec![1,3,6,10,15,21]));
+        assert_eq!(68, diff_tree(&vec![10,13,16,21,30,45]));
     }
 }
