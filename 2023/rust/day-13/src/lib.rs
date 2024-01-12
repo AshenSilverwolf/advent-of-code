31189b87959607d55e0554b249965501d6fb0d4e
AshenSilverwolf <gilbert.gavin@outlook.com>
2024-01-12 15:52:30 +0000

    2023 Day 13 Parsing
    brute force only way?

--- 2023/rust/day-13/src/lib.rs
+++ 2023/rust/day-13/src/lib.rs
@@ -1,4 +1,42 @@
+use nom::{
+    IResult,
+    multi::{separated_list1, many1},
+    character::complete::{newline, one_of},
+    combinator::map,
+    sequence::tuple,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Ground {
+    Ash,
+    Rock,
+}
+
+type Pattern = Vec<Vec<Ground>>;
+
+impl From<char> for Ground {
+    fn from(c: char) -> Self {
+        match c {
+            '.' => Ground::Ash,
+            '#' => Ground::Rock,
+            _ => unreachable!(),
+        }
+    }
+}
+
+fn pattern(input: &str) -> IResult<&str, Vec<Vec<Ground>>> {
+    separated_list1(newline, many1(map(one_of(".#"), Ground::from)))(input)
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<Pattern>> {
+    separated_list1(tuple((newline, newline)), pattern)(input)
+}
+
 pub fn process_part1(input: &str) -> String {
+    let (_, patterns) = parse_input(input).expect("valid input");
+
+    dbg!(patterns);
+    
     todo!()
 }
 
@@ -10,7 +48,21 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "#.##..##.
+..#.##.#.
+##......#
+##......#
+..#.##.#.
+..##..##.
+#.#.##.#.
+
+#...##..#
+#....#..#
+..##..###
+#####.##.
+#####.##.
+..##..###
+#....#..#";
 
     #[test]
     fn part1_works() {
