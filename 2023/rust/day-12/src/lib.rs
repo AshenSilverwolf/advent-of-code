f3fd262318a23a2056904dd4964b494968b56c9e
AshenSilverwolf <gilbert.gavin@outlook.com>
2024-01-12 15:25:22 +0000

    2023 Day 12 Input Parsing
    this is gonna be a tough one
    feels very similar to a battleship-type algorithm

--- 2023/rust/day-12/src/lib.rs
+++ 2023/rust/day-12/src/lib.rs
@@ -1,4 +1,54 @@
+use nom::{
+    IResult,
+    character::complete::{self, newline, one_of},
+    multi::{separated_list1, many1},
+    combinator::map,
+    bytes::complete::tag,
+    sequence::separated_pair,
+};
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+enum Condition {
+    Operational,
+    Damaged,
+    Unknown,
+}
+
+impl From<char> for Condition {    
+    fn from(value: char) -> Self {
+        match value {
+            '.' => Condition::Operational,
+            '#' => Condition::Damaged,
+            '?' => Condition::Unknown,
+            _ => panic!("invalid condition conversion"),
+        }
+    }
+}
+
+fn record(input: &str) -> IResult<&str, Vec<Condition>> {
+    many1(map(one_of(".#?"), Condition::from))(input)
+}
+
+fn sequence(input: &str) -> IResult<&str, Vec<u32>> {
+    separated_list1(tag(","), complete::u32)(input)
+}
+
+fn parse_input(input: &str) -> IResult<&str, Vec<(Vec<Condition>, Vec<u32>)>> {
+    separated_list1(
+        newline,
+        separated_pair(
+            record,
+            tag(" "),
+            sequence,
+        ),
+    )(input)
+}
+
 pub fn process_part1(input: &str) -> String {
+    let (_, spring_rows) = parse_input(input).expect("valid input");
+
+    dbg!(spring_rows);
+
     todo!()
 }
 
@@ -10,16 +60,22 @@ pub fn process_part2(input: &str) -> String {
 mod tests {
     use super::*;
 
-    const INPUT: &str = "";
+    const INPUT: &str = "???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1";
 
     #[test]
     fn part1_works() {
-        let expected = String::from("");
+        let expected = String::from("21");
         let result = process_part1(INPUT);
         assert_eq!(expected, result);
     }
 
     #[test]
+    #[ignore]
     fn part2_works() {
         let expected = String::from("");
         let result = process_part2(INPUT);
